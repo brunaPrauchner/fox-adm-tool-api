7d1bfbdc88772a9b2f9f4bd24627764e57bb2137
brunaPrauchner <bruna.prauchner@acad.pucrs.br>
2021-02-16 16:05:29 -0300

    get and post completed

---
+++ FoxAdmTool.API/Controllers/ProductsController.cs
@@ -0,0 +1,76 @@
+﻿using FoxAdmTool.API.Models;
+using FoxAdmTool.API.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxAdmTool.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsService _productsService;
+
+        public ProductsController(IProductsService productsService)
+        {
+            _productsService = productsService;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _productsService.GetAsync(id);
+            return Ok(product);
+        }
+        
+        [HttpGet] 
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var product = await _productsService.GetAllAsync();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostProduct([FromBody] Product product)
+        {
+            try
+            {
+                await _productsService.AddProduct(product);
+                return Ok(product);
+            } catch (Exception ex)
+            {
+                return StatusCode(500);
+            }  
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var response = await _productsService.DeleteProductAsync(id);
+            return Ok(response);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ FoxAdmTool.API/Service/ProductsService.cs
@@ -0,0 +1,67 @@
+﻿using FoxAdmTool.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxAdmTool.API.Service
+{
+
+    public interface IProductsService
+    {
+        Task<Product> GetAsync(int id);
+        Task<List<Product>> GetAllAsync();
+        //Task<bool> AddProductAsync(Product product);
+        Task<bool> AddProduct(Product product);
+        Task<bool> DeleteProductAsync(int id);
+    }
+    public class ProductsService : IProductsService
+    {
+        private readonly ShopContext _context;
+        public ProductsService(ShopContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        public async Task<Product> GetAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if(product == null)
+            {
+                throw new Exception("product doesnt exist ");
+            }
+            return product;
+        }
+        public async Task<List<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<bool> AddProduct(Product product)
+        {
+            //var result = _context.Products.AddAsync(product);
+            //await _context.SaveChangesAsync();
+            //return product;
+            var result = _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return result.IsCompleted;
+
+        }
+
+
+        public async Task<bool> DeleteProductAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if(product == null)
+            {
+                throw new Exception("product doesnt exist ");
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
